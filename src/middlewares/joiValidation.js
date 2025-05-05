a3ab8ba6de0422ea0730c6f8dc603202474c7b1c
anuska <anuskasth2022@gmail.com>
2025-05-05 12:57:47 +1000

    update book fix

--- src/middlewares/joiValidation.js
+++ src/middlewares/joiValidation.js
@@ -27,7 +27,6 @@ export const registerValidator = (req, res, next) => {
     phone: Joi.string(),
     email: Joi.string().email({ minDomainSegments: 2 }),
     password: Joi.string(),
-    
   });
   joiValidator(registerSchema, req, res, next);
 };
@@ -50,16 +49,13 @@ export const createBookValidator = (req, res, next) => {
 export const updateBookValidator = (req, res, next) => {
   const updateBookSchema = Joi.object({
     title: Joi.string().required(),
-    _id: Joi.string().required(),
     status: Joi.string().required(),
     author: Joi.string().required(),
     thumbnail: Joi.string().required(),
-    isbn: Joi.string().required(),
     genre: Joi.string().required(),
     description: Joi.string().required(),
     publishedYear: Joi.number().required(),
-    isAvailable: Joi.boolean().required(),
-    expectedAvailable: Joi.string().allow("", null),
+    availabiliy: Joi.boolean(),
   });
   joiValidator(updateBookSchema, req, res, next);
 };
